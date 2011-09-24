59f38d0fe41e01f873289fbae390db9bbafa9c74
Shionoya Risa <Shio@txsi.co.uk>
2011-09-24 04:07:25 +0100

    A bit more housekeeping and README update

--- includes/version.php
+++ includes/version.php
@@ -1,3 +1,3 @@
 <?php
-define('_SKILLSHEET_VERSION', 'v4.Incarna.167');
+define('_SKILLSHEET_VERSION', 'v4.Incarna.168');
 ?>
--- themes/default/plugins/function.topsps.php
+++ themes/default/plugins/function.topsps.php
@@ -1,10 +1,20 @@
 <?php
-/**
- * $Id$
- * Smarty plugin
- * @package Skillsheet
- * @subpackage plugins
- */
+/***********************************************************/
+/*           EVE Character Showroom - Version 4            */
+/*       'Improved' and maintained by Shionoya Risa        */
+/*          Originally created by DeTox MinRohim           */
+/***********************************************************/
+/*   This thing is free for you to take. You take all      */
+/* responsabilities for using it. Whatever you do with it, */
+/*  I don't care (although I would appreciate you send me  */
+/*   any enhancement - ISK donations are also accepted).   */
+/*                                                         */
+/*   Lots of code snippets have been found in my travels   */
+/*    if you think one of those snippets is yours, tell    */
+/*         me. I will give all appropriate credits.        */
+/***********************************************************/
+/*   ISK donations to Shionoya Risa are also accepted ;)   */
+/***********************************************************/
 
 function smarty_function_topsps($params, &$smarty)
 {
