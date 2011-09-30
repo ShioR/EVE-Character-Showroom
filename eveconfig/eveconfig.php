3fdf87aaab037b8c3042a18b6293df9d74a97b8d
Shionoya Risa <Shio@txsi.co.uk>
2011-09-30 12:26:30 +0100

    Added XML to the cache folder and hopefully secured it. Changed xml cache naming slightly Renamed the cron jobs Spruced up the index.html's

--- eveconfig/eveconfig.php
+++ eveconfig/eveconfig.php
@@ -39,8 +39,8 @@ define('_INSTALLED', false);
 define('_SHORTURLS', true);
 // With trailing slash!
 define('_DOMAIN', '');
-// No trailing slash! - Please make this secure (Above webroot) since it will contain raw xml data from the EVE API!
-define('_XMLPATH', '/path/to/cron/cache/folder');
+// No trailing slash! Please edit this to the FULL path of the xml folder
+define('_XMLPATH', '/home/example/public_html/cache/xml');
 
 // Path to image caches
 $eve_filepath = 'cache/templates'; // Remember to chmod it 777
--- imgs/index.php
+++ imgs/index.php
@@ -1,1 +1,1 @@
-You probably shouldn't be here, you know that, right?
+You have a problem with authority, Mr. Anderson. You believe you are special, that somehow the rules do not apply to you. Obviously, you are mistaken. 
