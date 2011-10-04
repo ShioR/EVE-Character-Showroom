14b9a7ff4c37bfe16b2f9699f0ad2bd9d146d977
Shionoya Risa <Shio@txsi.co.uk>
2011-10-04 14:19:09 +0100

    Fixed an issue with the Daytrading skillcost information

--- includes/skilltree.php
+++ includes/skilltree.php
@@ -2629,7 +2629,7 @@ $skillsource[274][25233]['groupName'] = 'Trade';
 $skillsource[274][16595]['typeName'] = 'Daytrading';
 $skillsource[274][16595]['rank'] = '1';
 $skillsource[274][16595]['typeID'] = '16595';
-$skillsource[266][16595]['skillCost'] = '12500000';
+$skillsource[274][16595]['skillCost'] = '12500000';
 $skillsource[274][16595]['groupID'] = '274';
 $skillsource[274][16595]['groupName'] = 'Trade';
 
--- includes/version.php
+++ includes/version.php
@@ -1,3 +1,3 @@
 <?php
-define('_SKILLSHEET_VERSION', 'v4.182');
+define('_SKILLSHEET_VERSION', 'v4.183');
 define('_DATA_VERSION', 'Incarna');
