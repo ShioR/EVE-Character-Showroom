fe5eacb364d9632e681d0f8c252b61d30c7abdaa
Shionoya Risa <Shio@txsi.co.uk>
2012-01-20 13:01:09 +0000

    Updated to play nicely with Piro, it was previously not throwing the ball.

--- themes/default/js/js.js
+++ themes/default/js/js.js
@@ -1,6 +1,4 @@
-function showBox(){$('overlay').show();center('box');return false;}
-function hideBox(){$('box').hide();$('overlay').hide();return false;}
-function center(element){try{element=$(element);}catch(e){return;}
+
 var my_width=0;var my_height=0;if(typeof(window.innerWidth)=='number'){my_width=window.innerWidth;my_height=window.innerHeight;}else if(document.documentElement&&(document.documentElement.clientWidth||document.documentElement.clientHeight)){my_width=document.documentElement.clientWidth;my_height=document.documentElement.clientHeight;}
 else if(document.body&&(document.body.clientWidth||document.body.clientHeight)){my_width=document.body.clientWidth;my_height=document.body.clientHeight;}
 element.style.position='absolute';element.style.zIndex=99;var scrollY=0;if(document.documentElement&&document.documentElement.scrollTop){scrollY=document.documentElement.scrollTop;}else if(document.body&&document.body.scrollTop){scrollY=document.body.scrollTop;}else if(window.pageYOffset){scrollY=window.pageYOffset;}else if(window.scrollY){scrollY=window.scrollY;}
