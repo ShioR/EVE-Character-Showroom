2735fe9742ba95bf09adac248a16124d52c94cc4
Shionoya Risa <Shio@txsi.co.uk>
2012-02-25 23:14:55 +0000

    Modified getskillinfo
    Slightly increased the character count before a new line is created for
    skill descriptions.

--- includes/plugins/function.getskillinfo.php
+++ includes/plugins/function.getskillinfo.php
@@ -1,21 +1,4 @@
 <?php
-/***********************************************************/
-/*           EVE Character Showroom - Version 4            */
-/*       'Improved' and maintained by Shionoya Risa        */
-/*          Originally created by DeTox MinRohim           */
-/***********************************************************/
-/*   This thing is free for you to take. You take all      */
-/* responsabilities for using it. Whatever you do with it, */
-/*  I don't care (although I would appreciate you send me  */
-/*   any enhancement - ISK donations are also accepted).   */
-/*                                                         */
-/*   Lots of code snippets have been found in my travels   */
-/*    if you think one of those snippets is yours, tell    */
-/*         me. I will give all appropriate credits.        */
-/***********************************************************/
-/*   ISK donations to Shionoya Risa are also accepted ;)   */
-/***********************************************************/
-
 function smarty_function_getskillinfo($params, &$smarty)
 {
     global $eve;
@@ -28,7 +11,7 @@ function smarty_function_getskillinfo($params, &$smarty)
 				$result = mysql_query("SELECT description FROM data_invTypes WHERE typeID = '$typeID'");
 		// Wordwrap to stop the page getting borked
 				$text = mysql_result($result, 0);
-				$description = wordwrap($text, 125, "<br />\n");
+				$description = wordwrap($text, 130, "<br />\n");
 				echo $description, "<br />\n";
    	mysql_close();
 		// Fini!
