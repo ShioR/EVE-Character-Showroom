aabe972dd360420c74da89a23e0dbe2ffaa93277
Shionoya Risa <Shio@txsi.co.uk>
2012-02-26 10:13:02 +0000

    Changed alignment of text affected by wordwrap.

--- includes/plugins/function.getskillinfo2.php
+++ includes/plugins/function.getskillinfo2.php
@@ -11,7 +11,7 @@ function smarty_function_getskillinfo2($params, &$smarty)
 				$result = mysql_query("SELECT description FROM data_invTypes WHERE typeID = '$typeID'");
 		// Wordwrap to stop the page getting borked
 				$text = mysql_result($result, 0);
-				$description = wordwrap($text, 110, "<br />\n &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+				$description = wordwrap($text, 110, "<br />\n&nbsp;&nbsp;");
 				echo $description, "<br />\n";
    	mysql_close();
 		// Fini!
