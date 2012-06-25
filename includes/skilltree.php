6c63a90e29913bdb461d41fdcff97c152ab1aafe
Shionoya Risa <Shio@txsi.co.uk>
2012-06-25 19:47:34 +0100

    Added ID for Armor Resistance Phasing

--- includes/skilltree.php
+++ includes/skilltree.php
@@ -1078,12 +1078,12 @@ $skillsource[258][11574]['groupID'] = '258';
 $skillsource[258][11574]['groupName'] = 'Leadership';
 
 ////////////////////////////////////////////////////////////////////////// MECHANICS
-$skillsource[269][xxxx]['typeName'] = 'Armor Resistance Phasing';
-$skillsource[269][xxxx]['rank'] = '3';
-$skillsource[269][xxxx]['typeID'] = 'xxxx';
-$skillsource[269][xxxx]['skillCost'] = '500000';
-$skillsource[269][xxxx]['groupID'] = '269';
-$skillsource[269][xxxx]['groupName'] = 'Mechanics';
+$skillsource[269][32797]['typeName'] = 'Armor Resistance Phasing';
+$skillsource[269][32797]['rank'] = '3';
+$skillsource[269][32797]['typeID'] = '32797';
+$skillsource[269][32797]['skillCost'] = '500000';
+$skillsource[269][32797]['groupID'] = '269';
+$skillsource[269][32797]['groupName'] = 'Mechanics';
 
 $skillsource[269][26253]['typeName'] = 'Armor Rigging';
 $skillsource[269][26253]['rank'] = '3';
