dfa27cc988304aee04de73ee0aefb417ff8daefd
Shionoya Risa <Shio@txsi.co.uk>
2012-06-25 18:40:41 +0100

    Updated "Target Breaker" skill name.

--- includes/skilltree.php
+++ includes/skilltree.php
@@ -303,7 +303,7 @@ $skillsource[272][3551]['skillCost'] = '36000';
 $skillsource[272][3551]['groupID'] = '272';
 $skillsource[272][3551]['groupName'] = 'Electronics';
 
-$skillsource[272][19921]['typeName'] = 'Target Breaker';
+$skillsource[272][19921]['typeName'] = 'Target Breaker Amplification';
 $skillsource[272][19921]['rank'] = '5';
 $skillsource[272][19921]['typeID'] = '4411';
 $skillsource[272][19921]['skillCost'] = '600000';
