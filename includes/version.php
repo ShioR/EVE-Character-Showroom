332b4474e73a20075d271569da5ea79f43cc554e
Shionoya Risa <Shio@txsi.co.uk>
2015-06-10 13:11:48 +0100

    Build 315 - Carynx

--- includes/version.php
+++ includes/version.php
@@ -1,9 +1,4 @@
 <?php
-<<<<<<< HEAD
 define('_SKILLSHEET_VERSION', 'v5.315');
 define('_DATA_VERSION', 'Carnyx');
-=======
-define('_SKILLSHEET_VERSION', 'v5.314');
-define('_DATA_VERSION', 'Tiamat');
->>>>>>> origin/master
 ?>
