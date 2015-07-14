6eaa4867f5111a829ae1eaa37115f49c55471844
Shionoya Risa <Shio@txsi.co.uk>
2015-07-14 19:48:13 +0100

    eveconfig.php -> config.php

--- includes/plugins/function.getdomain.php
+++ includes/plugins/function.getdomain.php
@@ -2,7 +2,7 @@
 function smarty_function_getdomain($params, &$smarty)
 {
     global $eve;
-    require_once 'eveconfig/eveconfig.php';
+    include(dirname(__FILE__).'/../config.php');
     $domain = _DOMAIN;
     echo $domain;
 }
