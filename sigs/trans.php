71aabb760bad2afa98bd427087095c306298a425
Shionoya Risa <Shio@txsi.co.uk>
2015-07-15 23:05:29 +0100

    Updated to include the new transparent 'bg' image

--- sigs/trans.php
+++ sigs/trans.php
@@ -6,16 +6,15 @@ $domain = _DOMAIN;
 // Set the content-type
 header('Content-type: image/png');
 
-// Create the image out of thin air!
-$im = imagecreatetruecolor(630, 25);
+// Create the image
+//$im = imagecreatetruecolor(630, 25);
+$im = imagecreatefrompng('bg/trans.png');
 
 // Create some colors
 $darkgrey = imagecolorallocatealpha($im, 27, 27, 27, 0);
-$whitebg = imagecolorallocatealpha($im, 255, 255, 255, 25);
 $white = imagecolorallocate($im, 255, 255, 255);
 $black = imagecolorallocate($im, 0, 0, 0);
-$grey = imagecolorallocatealpha($im, 150, 150, 150, 0);
-$gold = imagecolorallocatealpha($im, 255, 215, 0, 0);
+
 imagecolortransparent ($im, $white);
 imagefilledrectangle($im, 0, 0, 630, 25, $white);
 
@@ -27,11 +26,9 @@ $font = 'arial.ttf';
 
 // Add a shadow to the text if the user wants it
 if (isset($_GET["s"]) == "y") {
-imagettftext($im, 9, 0, 1, 17, $darkgrey, $font, $text);
-//imagefilter($im, IMG_FILTER_GAUSSIAN_BLUR);
-//imagettftext($im, 9, 0, 3, 16, $black, $font, $text);
+imagettftext($im, 9, 0, 6, 17, $darkgrey, $font, $text);
 } else {
-imagettftext($im, 9, 0, 0, 16, $black, $font, $text);
+imagettftext($im, 9, 0, 5, 16, $black, $font, $text);
 }
 
 // Using imagepng() results in clearer text compared with imagejpeg()
