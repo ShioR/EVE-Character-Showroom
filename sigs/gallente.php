cdefbd96fe10db94d8dd95a3f717f27bcb1babdc
Shionoya Risa <Shio@txsi.co.uk>
2015-07-22 21:20:48 +0100

    Signature update;
    * Fixes when portrait missing, signatures doesn't load
    * Removes excess code

--- sigs/gallente.php
+++ sigs/gallente.php
@@ -1,55 +1,73 @@
 <?php 
-// Fetch the domain from the config
+/**********************************************************/
+/*           EVE Character Showroom - Version 5           */
+/*       'Improved' and maintained by Shionoya Risa       */
+/*          Originally created by DeTox MinRohim          */
+/*            Copyright (C) 2015 Shionoya Risa            */
+/**********************************************************/
+/* This program is free software: you can redistribute it */
+/*   and/or modify it under the terms of the GNU General  */
+/*     Public License as published by the Free Software   */
+/*     Foundation, either version 3 of the License, or    */
+/*           (at your option) any later version.          */
+/*  This program is distributed in the hope that it will  */
+/*  be useful, but WITHOUT ANY WARRANTY; without even the */
+/*   implied warranty of MERCHANTABILITY or FITNESS FOR   */
+/*                  A PARTICULAR PURPOSE.                 */
+/*  See the GNU General Public License for more details.  */
+/*         You should have received a copy of the         */
+/*   GNU General Public License along with this program.  */
+/*        If not, see http://www.gnu.org/licenses/        */
+/**********************************************************/
+/*   Lots of code snippets have been found in my travels  */
+/*    if you think one of those snippets is yours, tell   */
+/*         me. I will give all appropriate credits.       */
+/**********************************************************/
+/*  All EVE Online logos, images, trademarks and related  */
+/* materials are copyright (C) CCP hf http://ccpgames.com */
+/**********************************************************/
+
+// Includes
 include "../includes/config.php";
 include "../includes/plugins/function.getsigportrait.php";
-$domain = _DOMAIN;
 
-// Set the content-type
+$name = stripslashes($_GET['n']);       // Grab this from the query string
+$nafn = str_replace('_', ' ', $name);   // MySQL friendly name (replaces any underscores with a space)
+$domain = _DOMAIN;                      // Not needed, but easier to remember if I do it like this
+$sigName = 'gallente';                  // Used for for loading the bg image
+
+// Set the content-type with header
 header('Content-type: image/jpeg');
 
 // Create the image from the bg
-$im = imagecreatefromjpeg('bg/gallente.jpg');
+$im = imagecreatefromjpeg('bg/'.$sigName.'.jpg');
 
-// Create some colors
-$black = imagecolorallocate($im, 0, 0, 0);
-$black1 = imagecolorallocatealpha($im, 7, 7, 7, 10);
+// Specify some colors
 $white = imagecolorallocate($im, 255, 255, 255);
 $darkgrey = imagecolorallocatealpha($im, 40, 40, 40, 50);
-$grey = imagecolorallocate($im, 150, 150, 150);
-$gold = imagecolorallocate($im, 255, 215, 0);
 
-// Get the signature text from the URL
-$name = stripslashes($_GET['n']);
-// MySQL friendly name (replace the underscore for a space)
-$navn = str_replace('_', ' ', $name);
-
-// The signature .tpl output url
-$text = file_get_contents ($domain.$name.'/newsig');
+// The signature text URL
+$text = file_get_contents ($domain.$name.'/newsig'); 
 // Location of the font to use
 $font = 'din1451e.ttf';
 
-// Add a shadow & the text 
+// Shadow & the text 
 imagettftext($im, 10.5, 0, 77, 20, $darkgrey, $font, $text);
 imagettftext($im, 10.5, 0, 75, 18, $white, $font, $text);
 
 // Fetch the characterID from the database
     // Connect to DB using mysqli
-        $mysqli = mysqli_connect($dbconfig['dbhost'], $dbconfig['dbuname'], $dbconfig['dbpass'], $dbconfig['dbname']);
-        $query = "SELECT skillsheet_apis.characterID FROM skillsheet_apis WHERE name = '$navn'";
-        $result = mysqli_query($mysqli, $query);
+        $db = mysqli_connect($dbconfig['dbhost'], $dbconfig['dbuname'], $dbconfig['dbpass'], $dbconfig['dbname']);
+        $query = "SELECT characterID FROM skillsheet_apis WHERE name = '$nafn'";
+        $result = mysqli_query($db, $query);
         $out = mysqli_fetch_row($result);
         $id = $out[0];
     // Free result & close connection
         mysqli_free_result($result);
-   	    mysqli_close($mysqli);
+   	    mysqli_close($db);
 
-// Fetch portrait
-    if (file_exists('../cache/characters/'.$id.'_256.jpg')) {
-        $portrait = imagecreatefromjpeg(getportrait());
-    } else {
-        $portrait = imagecreatefromjpeg('https://image.eveonline.com/Character/'.$id.'_64.jpg');
-    }
-
+// Fetch portrait & add it to the image
+$portrait = imagecreatefromjpeg(getportrait($id));
 imagecopyresampled($im, $portrait, 6, 8, 0, 0, 64, 64, 64, 64);
 
 // Use imagejpeg() to create the image with 95% quality
