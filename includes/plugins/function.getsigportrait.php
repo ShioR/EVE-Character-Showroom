e71e9ece3955758af2a37d68ff3af55c322cb901
Shionoya Risa <Shio@txsi.co.uk>
2015-07-22 21:15:31 +0100

    + License info

--- includes/plugins/function.getsigportrait.php
+++ includes/plugins/function.getsigportrait.php
@@ -1,4 +1,32 @@
 <?php
+/**********************************************************/
+/*           EVE Character Showroom - Version 5           */
+/*       'Improved' and maintained by Shionoya Risa       */
+/*          Originally created by DeTox MinRohim          */
+/*            Copyright (C) 2015 Shionoya Risa            */
+/**********************************************************/
+/* This program is free software: you can redistribute it */
+/*   and/or modify it under the terms of the GNU General  */
+/*     Public License as published by the Free Software   */
+/*     Foundation, either version 3 of the License, or    */
+/*           (at your option) any later version.          */
+/*  This program is distributed in the hope that it will  */
+/*  be useful, but WITHOUT ANY WARRANTY; without even the */
+/*   implied warranty of MERCHANTABILITY or FITNESS FOR   */
+/*                  A PARTICULAR PURPOSE.                 */
+/*  See the GNU General Public License for more details.  */
+/*         You should have received a copy of the         */
+/*   GNU General Public License along with this program.  */
+/*        If not, see http://www.gnu.org/licenses/        */
+/**********************************************************/
+/*   Lots of code snippets have been found in my travels  */
+/*    if you think one of those snippets is yours, tell   */
+/*         me. I will give all appropriate credits.       */
+/**********************************************************/
+/*  All EVE Online logos, images, trademarks and related  */
+/* materials are copyright (C) CCP hf http://ccpgames.com */
+/**********************************************************/
+
 // Yes, I know this actually pulls all sizes, but the benefit outweighs the cost imo.
 function getportrait($id)
 {
