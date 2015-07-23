ba25f1ea8a755b7037dfb7458370d962447b6e6e
Shionoya Risa <Shio@txsi.co.uk>
2015-07-23 19:22:31 +0100

    New config blank
    Shuffled a few things around

--- includes/config.php
+++ includes/config.php
@@ -1,20 +1,31 @@
 <?php
-/***********************************************************/
-/*           EVE Character Showroom - Version 5            */
-/*       'Improved' and maintained by Shionoya Risa        */
-/*          Originally created by DeTox MinRohim           */
-/***********************************************************/
-/*   This thing is free for you to take. You take all      */
-/* responsibilities for using it. Whatever you do with it, */
-/*  I don't care (although I would appreciate you send me  */
-/*   any enhancement - ISK donations are also accepted).   */
-/*                                                         */
-/*   Lots of code snippets have been found in my travels   */
-/*    if you think one of those snippets is yours, tell    */
-/*         me. I will give all appropriate credits.        */
-/***********************************************************/
-/*   ISK donations to Shionoya Risa are also accepted ;)   */
-/***********************************************************/
+/**********************************************************/
+/*           EVE Character Showroom - Version 5           */
+/*       'Improved' and maintained by Shionoya Risa       */
+/*          Originally created by DeTox MinRohim          */
+/*            Copyright (C) 2015 Shionoya Risa            */
+/**********************************************************/
+/* This program is free software: you can redistribute it */
+/*   and/or modify it under the terms of the GNU General  */
+/*     Public License as published by the Free Software   */
+/*     Foundation, either version 3 of the License, or    */
+/*           (at your option) any later version.          */
+/*  This program is distributed in the hope that it will  */
+/*  be useful, but WITHOUT ANY WARRANTY; without even the */
+/*   implied warranty of MERCHANTABILITY or FITNESS FOR   */
+/*                  A PARTICULAR PURPOSE.                 */
+/*  See the GNU General Public License for more details.  */
+/*         You should have received a copy of the         */
+/*   GNU General Public License along with this program.  */
+/*        If not, see http://www.gnu.org/licenses/        */
+/**********************************************************/
+/*   Lots of code snippets have been found in my travels  */
+/*    if you think one of those snippets is yours, tell   */
+/*         me. I will give all appropriate credits.       */
+/**********************************************************/
+/*  All EVE Online logos, images, trademarks and related  */
+/* materials are copyright (C) CCP hf http://ccpgames.com */
+/**********************************************************/
 
 $dbconfig['dbtype'] = 'mysqli';
 $dbconfig['dbtabletype'] = 'MyISAM';
@@ -23,30 +34,33 @@ $dbconfig['dbuname'] = '';
 $dbconfig['dbpass'] = '';
 $dbconfig['dbname'] = '';
 
-
 $dbconfig['system']      = '0';
 $dbconfig['prefix']      = '';
 $dbconfig['encoded'] 	 = '0';
 $dbconfig['pconnect']	 = '0';
 $dbconfig['temp']        = 'cache';
 
+define('_INSTALLED', false);
+define('_SHORTURLS', true);
+define('_SHEETADMIN', '');
+// Make sure you include 'http://' and the trailing slash '/' or signatures WILL NOT work.
+define('_DOMAIN', '');
+// No trailing slash!
+define('_XMLPATH', '../cache/xml');
+
+// Path for templates and xml files - MAKE THAT NON-BROWSABLE !
+$eve_filepath = '../cache/templates'; // Remember to chmod it 777
+
+// Done... no need to touch anything else. (Unless you know what you are doing)
+///////////////////////////////////////////////////////////
+
 global $evedebug;
 $evedebug['debug']          = 0;
 $evedebug['debug_sql']      = 0;
 $evedebug['pagerendertime'] = 0;
 
-define('_INSTALLED', false);
-define('_SHORTURLS', true);
-// Make sure you include 'http://' and the trailing slash '/' or signatures WILL NOT work.
-define('_DOMAIN', 'http://example.com/');
-
-// Path to template cache
-$eve_filepath = 'cache/templates'; // Remember to chmod it 777
-
-// Done... no need to touch anything else. (Unless you know what you are doing)
-///////////////////////////////////////////////////////////
-
-//$eveconfig['default']    = $default;
-//$eveconfig['eveconnect'] = $eveconnect;
+$eveconfig['default']    = $default;
 $eveconfig['filepath']   = $eve_filepath;
+$eveconfig['eveconnect'] = $eveconnect;
+
 ?>
