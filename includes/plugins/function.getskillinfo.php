590a57f5e50d55096f15abe2b307c62e7fa34d0b
Shionoya Risa <Shio@txsi.co.uk>
2016-04-06 19:51:47 +0100

    Fixes misalignment of skill description

--- includes/plugins/function.getskillinfo.php
+++ includes/plugins/function.getskillinfo.php
@@ -3,7 +3,7 @@
 /*           EVE Character Showroom - Version 5           */
 /*       'Improved' and maintained by Shionoya Risa       */
 /*          Originally created by DeTox MinRohim          */
-/*            Copyright (C) 2015 Shionoya Risa            */
+/*            Copyright (C) 2016 Shionoya Risa            */
 /**********************************************************/
 /* This program is free software: you can redistribute it */
 /*   and/or modify it under the terms of the GNU General  */
@@ -39,7 +39,9 @@ function smarty_function_getskillinfo($params, &$smarty)
             $query = "SELECT description FROM skillsheet_skills WHERE typeID = '$typeID'";
             $result = mysqli_query($connect, $query);
             $output = mysqli_fetch_row($result);
-            $description = wordwrap($output[0], 112, "<br />");
+            $description = wordwrap($output[0], 110, "<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+        // Strip the <b> tag from the description
+            $description = str_replace(array('<b>'), '', $description);
             echo nl2br($description);
         // Extra line break at the end to put the skill cost onto a new line
             echo "<br />";
