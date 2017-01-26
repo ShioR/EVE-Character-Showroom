0d6588e89d63cae5edf590717281337fa641a5de
Shionoya Risa <Shio@txsi.co.uk>
2017-01-26 18:40:04 +0000

    Build 428

--- includes/version.php
+++ includes/version.php
@@ -27,6 +27,6 @@
 /* materials are copyright (C) CCP hf http://ccpgames.com */
 /**********************************************************/
 
-define('_SKILLSHEET_VERSION', 'v6.427');
+define('_SKILLSHEET_VERSION', 'v6.428');
 define('_DATA_VERSION', 'Ascension');
 ?>
